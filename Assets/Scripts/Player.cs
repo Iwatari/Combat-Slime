9cd0f94eb8005ed346e0f13b58646fbcd315ed33
Ilya <121040260+Iwatari@users.noreply.github.com>
2024-10-06 18:28:08 +0700

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+namespace CombatSlime
+{
+    public class Player : SingletonBase<Player>
+    {
+
+    }
+}
